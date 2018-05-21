1a766cc1657407aea16b755ad51784205387a9a1
Vladimir Nechai <volodymyr.nechai@gmail.com>
2018-05-21 13:54:49 +0200

    import Chairs

--- app/Console/Commands/ImportFau.php
+++ app/Console/Commands/ImportFau.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\Chair;
+use App\Department;
 use App\Faculty;
 use Illuminate\Console\Command;
 use GuzzleHttp\Client;
@@ -57,45 +59,160 @@ class ImportFau extends Command
     {
         switch ($this->argument('type')) {
             case "faculties":
-
-                foreach(range('M','T') as $v) {
-                    try {
-                        $res = $this->client->get($this->endPoint, [
-                            'query' => [
-                                'search' => 'departments',
-//                                'id' => '49626763',
-//                                'path' => 'recht',
-                                'name' => "^$v",
-                                'show' => 'xml'
-                            ],
-                        ]);
-
-                        $xml = simplexml_load_string($res->getBody()->getContents());
-
-                        $list = $xml->xpath("//Org[not(contains(substring-after(@key, 'Org.'), '.'))]");
-                        foreach ($list as $org) {
-                            $faculty = new Faculty;
-                            $faculty->name = $org->name;
-                            $faculty->type = "Faculty";
-                            $faculty->university_id = 2;
-                            $faculty->univis_id = $org->id;
-                            $faculty->univis_orgnr = $org->attributes()['orgnr'];
-                            $faculty->univis_hash = hash('sha256', $org);
-                            $faculty->location = $org->ort;
-                            $faculty->save();
-
-                            echo $org->name . " has been imported";
-                        }
-                    } catch (Exception $e) {
-                        echo ($e->getMessage());
-                    }
-
-                }
-
-                echo "Faculties import finished";
+                $this->importFaculties();
+                break;
+            case "departments":
+                $this->importDepartments();
+                break;
+            case "chairs":
+                $this->importChairs();
                 break;
             default:
-                echo "input either 'faculties'";
+                echo "Something went wrong! Please try again";
         }
+    }
+
+    /**
+     * @return void
+     */
+    private function importFaculties() {
+        foreach(range('A','Z') as $v) {
+            try {
+                $res = $this->client->get($this->endPoint, [
+                    'query' => [
+                        'search' => 'departments',
+                        'name' => "^$v",
+                        'show' => 'xml'
+                    ],
+                ]);
+
+                $xml = simplexml_load_string($res->getBody()->getContents());
+
+                //TODO: fix hash attribute
+                $list = $xml->xpath("//Org[not(contains(substring-after(@key, 'Org.'), '.'))]");
+                foreach ($list as $org) {
+                    try {
+                        $faculty = new Faculty;
+                        $faculty->name = $org->name;
+                        $faculty->type = "Faculty";
+                        $faculty->university_id = 1;
+                        $faculty->univis_id = $org->id;
+                        $faculty->univis_orgnr = $org->attributes()['orgnr'];
+                        $faculty->univis_key = $org->attributes()['key'];
+                        $faculty->location = $org->ort;
+                        $faculty->save();
+
+                        echo $org->attributes()['key'] . " has been imported \n";
+
+                    } catch (\Exception $e) {
+                        echo ($e->getMessage());
+                    }
+                }
+            } catch (Exception $e) {
+                echo ($e->getMessage());
+            }
+        }
+
+        echo "Faculties import finished";
+    }
+
+    /**
+     * @return void
+     */
+    private function importDepartments()
+    {
+        $faculties = Faculty::all();
+        foreach ($faculties as $faculty) {
+            foreach (range('A', 'Z') as $v) {
+                try {
+                    $res = $this->client->get($this->endPoint, [
+                        'query' => [
+                            'search' => 'departments',
+                            'name' => "^$v",
+                            'show' => 'xml'
+                        ],
+                    ]);
+
+                    $xml = simplexml_load_string($res->getBody()->getContents());
+
+                    //TODO: fix hash attribute
+                    $list = $xml->xpath("//Org[not(contains(substring-after(@key, '" . $faculty->univis_key . ".'), '.')) and (contains(@key, '" . $faculty->univis_key . ".'))]");
+                    foreach ($list as $org) {
+                        try {
+                            $department = new Department;
+                            $department->name = $org->name;
+                            $department->type = "Department";
+                            $department->faculty_id = $faculty->id;
+                            $department->univis_id = $org->id;
+                            $department->univis_orgnr = $org->attributes()['orgnr'];
+                            $department->univis_key = $org->attributes()['key'];
+//                            $department->univis_hash = hash('sha256', $org->id);
+                            $department->location = $org->ort;
+                            $department->save();
+
+                            echo $org->attributes()['key'] . " has been imported \n";
+
+                        } catch (\Exception $e) {
+                            echo($e->getMessage());
+                        }
+                    }
+                } catch (Exception $e) {
+                    echo($e->getMessage());
+                }
+            }
+        }
+
+        echo "Departments import finished";
+    }
+
+    /**
+     * @return void
+     */
+    private function importChairs()
+    {
+        foreach(range('A','Z') as $v) {
+            try {
+                $res = $this->client->get($this->endPoint, [
+                    'query' => [
+                        'search' => 'departments',
+                        'name' => "^$v",
+                        'show' => 'xml'
+                    ],
+                ]);
+
+                $xml = simplexml_load_string($res->getBody()->getContents());
+
+                //TODO: fix hash attribute
+                $list = $xml->xpath("//Org");
+                foreach ($list as $org) {
+                    try {
+                        $key = $org->attributes()['key'];
+                        if (substr_count($key, '.') == 3) {
+                            $departmentKey = substr($key, 0, strpos($key, '.', strpos($key, '.', strpos($key, '.')+1)+1));
+                            $department = Department::where('univis_key', $departmentKey)->first();
+
+                            $chair = new Chair;
+                            $chair->name = $org->name;
+                            $chair->department_id = $department->id;
+                            $chair->univis_id = $org->id;
+                            $chair->univis_orgnr = $org->attributes()['orgnr'];
+                            $chair->univis_key = $org->attributes()['key'];
+                            $chair->location = $org->ort;
+                            $chair->save();
+
+
+                            echo $department->univis_key . ": " . $key . " has been imported \n";
+                        }
+
+                    } catch (\Exception $e) {
+                        echo ($e->getMessage());
+                    }
+                }
+            } catch (Exception $e) {
+                echo ($e->getMessage(). "\n");
+            }
+        }
+
+        echo "Chairs import finished";
     }
 }
