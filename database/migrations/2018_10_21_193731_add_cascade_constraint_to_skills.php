e8d845991e3cdaa1a4110712e7d1975434e14682
Vladimir Nechai <vne@adorsys.de>
2018-10-21 21:43:34 +0200

    cascade constrain to skill_course table

---
+++ database/migrations/2018_10_21_193731_add_cascade_constraint_to_skills.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCascadeConstraintToSkills extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('skill_course', function (Blueprint $table) {
+            // user_id
+            $table->dropForeign('skill_course_user_id_foreign');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            // course_id
+            $table->dropForeign('skill_course_course_id_foreign');
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('skill_course', function (Blueprint $table) {
+            // user_id
+            $table->dropForeign('skill_course_user_id_foreign');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            // course_id
+            $table->dropForeign('skill_course_course_id_foreign');
+            $table->foreign('course_id')->references('id')->on('courses');
+        });
+    }
+}
