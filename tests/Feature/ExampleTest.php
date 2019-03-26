653d600192eb2ecabfd54e50967e2b9bd8936a5e
Vladimir Nechai <volodymyr.nechai@fau.de>
2019-03-26 00:21:57 +0000

    ExampleTest.php edited online with Bitbucket

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,10 +3,12 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
+    use WithoutMiddleware;
     /**
      * A basic test example.
      *
