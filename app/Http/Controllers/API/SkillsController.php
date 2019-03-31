1386ce56acf4294d5b3f4f16cfb2e7bc4fee79a1
Vladimir Nechai <vne@adorsys.de>
2019-03-31 19:52:33 +0200

    add skill to swagger

--- app/Http/Controllers/API/SkillsController.php
+++ app/Http/Controllers/API/SkillsController.php
@@ -11,7 +11,30 @@ use Symfony\Component\HttpFoundation\Response as ResponseCode;
 class SkillsController extends Controller
 {
     /**
-     * Quick search skills for reviews and filters
+     * @OA\Get(
+     *     path="/skills/quick-search",
+     *     tags={"Quick search"},
+     *     summary="Quick search for skills. To be used for autocomplete",
+     *     description="Returns paginated skills collection.",
+     *     operationId="quickSearchSkills",
+     *     @OA\Parameter(
+     *         name="q",
+     *         in="query",
+     *         description="query param to search skills",
+     *         required=false,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation"
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Server error"
+     *     )
+     * )
      *
      * @param Request $request
      * @return Response
