09f87ba5ace69cbb77dd84b2b071f7f7888ce4c6
Vladimir Nechai <vne@adorsys.de>
2019-04-07 23:09:26 +0200

    add x-csrf-token header

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,7 +19,7 @@ class Cors
 
             header('Access-Control-Allow-Origin: *');
             header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-            header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
+            header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization, x-csrf-token');
             return $next($request);
     }
 }
