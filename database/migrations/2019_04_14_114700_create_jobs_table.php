f544a6a72fb9e02455e7b05c869873d3298fa853
Vladimir Nechai <vne@adorsys.de>
2019-04-14 13:59:00 +0200

    add job list timing migration

---
+++ database/migrations/2019_04_14_114700_create_jobs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('company_id');
+            $table->string('title', 100);
+            $table->enum('type', ['fulltime', 'student-job', 'internship'])->default('student-job');
+            $table->text('description');
+            $table->timestamps();
+
+            $table->foreign('company_id')->references('id')->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
