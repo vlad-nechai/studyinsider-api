fcede47e959e9b33eb90908d3720b1d9d3f86809
Vladimir Nechai <vne@adorsys.de>
2019-05-03 17:32:22 +0300

    cors fixes

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,9 +17,16 @@ class Cors
     {
         //TODO: allow only for localhost server ???
 
-            header('Access-Control-Allow-Origin: http:localhost:4200');
-            header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-            header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization, x-csrf-token');
-            return $next($request);
+//            header('Access-Control-Allow-Origin: http://localhost:4200');
+//            header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+//            header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization, x-csrf-token');
+//            return $next($request);
+
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', 'http://localhost:4200')
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT')
+            ->header('Access-Control-Max-Age', '3600')
+            ->header('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, X-Requested-With');
     }
 }
